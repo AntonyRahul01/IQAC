3ad5c57588369ff65855bc8b6825eb8859e37b31
JOHNSONSELVA115 <johnsonselva214@gmail.com>
2023-07-05 15:45:52 +0530

    loginfirst-john

---
+++ SD/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Login Page in HTML with CSS Code Example</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+		integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" href="./style.css">
+
+</head>
+
+<body>
+	<!-- partial:index.partial.html -->
+	<div class="box-form">
+		<div class="left">
+			<div class="overlay">
+			
+			</div>
+		</div>
+
+
+		<div class="right">
+			<h5 style=" font-weight:bolder;color: #d30a0a ;width: 200%;  ">ST.JOSEPH'S COLLEGE (AUTONOMOUS),</h5>
+			<h5 style=" font-weight:bolder;color: #d30a0a ;width: 200%;">TIRUCHIRAPPALLI-620002.</h5>
+			<br>
+			<h5>SCHOLAR LOGIN</h5>
+
+			<div class="inputs">
+				<input type="text" placeholder="user name">
+				<br>
+				<input type="password" placeholder="password">
+			
+			</div>
+
+			<br><br>
+
+			<div class="remember-me--forget-password">
+				<!-- Angular -->
+				<label>
+					<input type="checkbox" name="item" checked />
+					<span class="text-checkbox">Remember me</span>
+				</label>
+
+			</div>
+
+			<br>
+			<button>Login</button>
+		</div>
+
+	</div>
+	<!-- partial -->
+
+</body>
+
+</html>
