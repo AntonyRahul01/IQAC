d1967d300192cf7cdb28514051395046bc50d934
LeoDanielA01 <leodaniel6042005@gmail.com>
2023-07-19 22:00:24 +0530

    uploadfile database connected-leo

--- SD/dashboard.php
+++ SD/dashboard.php
@@ -2,9 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>:: Scholar Home ::</title>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="dstyle.css"/>
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
 </head>
 
 <body>
