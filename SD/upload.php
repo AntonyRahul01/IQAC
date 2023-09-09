a5546f7f8b8d8f8f701fbda740b7594ccdce5d6c
ReebaS <sreeba013@gmail.com>
2023-09-09 21:53:58 +0530

    upload

--- SD/upload.php
+++ SD/upload.php
@@ -1,27 +1,54 @@
-<?php include "../database/dbase.php";
-$name = $_POST['name'];
-$guidename = $_POST['guidename'];
-$journalname = $_POST['journalname'];
-$hindex = $_POST['hindex'];
-$type = $_POST['type'];
-$title = $_POST['title'];
-$impactfactor = $_POST['impactfactor'];
-$date = $_POST['date'];
-$authors = $_POST['authors'];
+<?php
+// Check if the form is submitted and the submit button is clicked
+echo "<script>alert('playing upload.php');</script>";
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["submit"])) {
+    // Include the database connection file
+    include "../database/dbase.php";
 
-$sql = "insert into journal_details( Name,GuideName,JournalName,HIndex,JournalType,PaperTitle,ImpactFactor,IssueDate,Authors) values(?,?,?,?,?,?,?,?,?)";
-$stmt = $cn->prepare($sql);
+    echo "<script>alert('playing if block');</script>";
 
-if ($stmt ===false)
-{
-    echo 'stmt failed';
+    // Get form data
+    $name = $_POST['name'];
+    $guidename = $_POST['guidename'];
+    $journalname = $_POST['journalname'];
+    $hindex = $_POST['hindex'];
+    $type = $_POST['type'];
+    $title = $_POST['title'];
+    $impactfactor = $_POST['impactfactor'];
+    $date = $_POST['date'];
+
+    // File upload handling
+    $target_directory = "../assets/";
+    $file_name = $_FILES['file']['name'];
+    $file_path = $target_directory . $file_name;
+
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $file_path)) {
+        // File uploaded successfully, now insert data into the database
+        $sql = "INSERT INTO journal_details (Name, GuideName, JournalName, HIndex, JournalType, PaperTitle, ImpactFactor, IssueDate, file)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $cn->prepare($sql);
+
+        if ($stmt === false) {
+            echo 'Statement preparation failed.';
+        }
+
+        // Bind parameters and execute the query
+        $stmt->bind_param("sssisssss", $name, $guidename, $journalname, $hindex, $type, $title, $impactfactor, $date, $file_path);
+
+        if ($stmt->execute()) {
+            echo "Data and file uploaded successfully!";
+        } else {
+            echo "Insertion failed. Error: " . $stmt->error;
+        }
+    } else {
+        echo "Failed to upload file.";
+    }
+
+    // Close the database connection
+    $cn->close();
 }
-$stmt->bind_param("sssisssss", $name, $guidename, $journalname, $hindex, $type, $title, $impactfactor, $date, $authors);
-
-if ($stmt->execute()) {
-    echo "Successfully inserted";
-} else {
-    echo "insertion failed";
+else{
+    
+    echo "<script>alert('playing else bl');</script>";
 }
-
-//$stmt = bind_param("sssisssss",)
+?>
--- SD/uploadfile.php
+++ SD/uploadfile.php
@@ -1,7 +1,3 @@
-<?php
-session_start();
-if ($_SESSION['s_id'] && $_SESSION['position'] == 'stud') {
-?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
@@ -24,69 +20,70 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'stud') {
     <div class="content_1">
       <div class="container">
         <header>Upload File</header>
+        <?php include "../database/dbase.php" ; ?>
 
-        <form action="upload.php" method="post">
-          <div class="form first">
-            <div class="details personal">
+        <form action="upload.php" method="POST" enctype="multipart/form-data">
 
-              <div class="fields">
-                <div class="input-field">
-                  <label>Full Name</label>
-                  <input type="text" name="name" placeholder="Enter your name" required>
-                </div>
-                <div class="input-field">
-                  <label>Guide Name</label>
-                  <input type="text" name="guidename" placeholder="Enter the name" required>
-                </div>
+          <div class="details personal">
 
-                <div class="input-field">
-                  <label>Journal Name</label>
-                  <input type="text" name="journalname"placeholder="Enter the name" required>
-                </div>
-
-                <div class="input-field">
-                  <label>H-Index</label>
-                  <input type="number" name="hindex" placeholder="Enter the H-Index" required>
-                </div>
-
-                <div class="input-field">
-                  <label>Journal Type</label>
-                  <select name="type" required>
-                    <option disabled selected>Select Journal</option>
-                    <option>UGC Care</option>
-                    <option>Scopus</option>
-                    <option>Web of Science</option>
-                    <option>Others</option>
-                  </select>
-                </div>
-
-                <div class="input-field">
-                  <label>Title of the Paper</label>
-                  <input type="text" name="title"placeholder="Enter the Title" required>
-                </div>
-
-                <div class="input-field">
-                  <label>Impact Factor</label>
-                  <input type="text" name="impactfactor"placeholder="Enter Impact Factor" required>
-                </div>
-                <div class="input-field">
-                  <label>Issued Date</label>
-                  <input type="date" name="date" placeholder="Enter your issued date" required>
-                </div>
-                <div class="input-field">
-                  <label>Authors</label>
-                  <input type="text" name="authors"placeholder="Enter the Authors" required>
-                </div>
-
-
-                <button class="sumbit">
-                  <span class="btnText">Submit</span>
-                </button>
-                <button class="clear" type="button" onclick="clearForm()">
-                  <span class="btnText">Clear</span>
-                </button>
-
+            <div class="fields">
+              <div class="input-field">
+                <label>Full Name</label>
+                <input type="text" name="name" placeholder="Enter your name" required>
               </div>
+              <div class="input-field">
+                <label>Guide Name</label>
+                <input type="text" name="guidename" placeholder="Enter the name" required>
+              </div>
+
+              <div class="input-field">
+                <label>Journal Name</label>
+                <input type="text" name="journalname" placeholder="Enter the name" required>
+              </div>
+
+              <div class="input-field">
+                <label>H-Index</label>
+                <input type="number" name="hindex" placeholder="Enter the H-Index" required>
+              </div>
+
+              <div class="input-field">
+                <label>Journal Type</label>
+                <select name="type" required>
+                  <option disabled selected>Select Journal</option>
+                  <option>UGC Care</option>
+                  <option>Scopus</option>
+                  <option>Web of Science</option>
+                  <option>Others</option>
+                </select>
+              </div>
+
+              <div class="input-field">
+                <label>Title of the Paper</label>
+                <input type="text" name="title" placeholder="Enter the Title" required>
+              </div>
+
+              <div class="input-field">
+                <label>Impact Factor</label>
+                <input type="text" name="impactfactor" placeholder="Enter Impact Factor" required>
+              </div>
+              <div class="input-field">
+                <label>Issued Date</label>
+                <input type="date" name="date" placeholder="Enter your issued date" required>
+              </div>
+              <div class="input-field">
+                <label>Pdf file</label>
+                <input type="file" name="file" accept="application/pdf" required>
+              </div>
+              <button class="clear" type="button" onclick="clearForm()">
+                <span class="btnText">Clear</span>
+              </button>
+
+              <button type="submit" name="submit" class="submit">
+                <span class="btnText">Submit</span>
+              </button>
+
+            </div>
+          </div>
         </form>
       </div>
 
@@ -105,8 +102,3 @@ if ($_SESSION['s_id'] && $_SESSION['position'] == 'stud') {
 </body>
 
 </html>
-<?php
-} else {
-    header("location:../Login/index.php");
-}
-?>
